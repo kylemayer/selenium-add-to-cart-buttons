a5c999352b515b9bf639943ca0b9b8965e39f309
Kyle Mayer <x@kylemayer.dev>
2021-08-08 21:09:55 -0700

    Dame merch website $D hat sold out

--- damedolla.py
+++ damedolla.py
@@ -4,11 +4,11 @@ from selenium import webdriver
 # For using Chrome
 browser = webdriver.Chrome('/Users/km/alchemy/chromedriver')
 
-# Amazon PS5 VR page
-browser.get('https://www.amazon.com/PlayStation-Marvels-Iron-Man-Bundle-4/dp/B08NRGF2DL/ref=sr_1_4?brr=1&dchild=1&qid=1628481134&rd=1&s=videogames&sr=1-4')
+# D.O.L.L.A. D$ Dad Hat page
+browser.get('https://www.bigdollamerch.com/product/5QCHDA002/d-dad-hat?cp=null')
 
-# BestBuy purchaseable page
-# browser.get('https://www.bestbuy.com/site/nvidia-geforce-rtx-nvlink-bridge-for-3090-cards-space-gray/6441554.p?skuId=6441554')
+# purchaseable page
+# browser.get('https://www.bigdollamerch.com/product/5QAMDA002/dolla-sign-mask?cp=null')
 
 buyButton = False
 
@@ -16,18 +16,18 @@ while not buyButton:
 
     try:
         # If this works then the button is not open
-        addToCartBtn = addButton = browser.find_element_by_class_name("btn-disabled")
+        addToCartBtn = addButton = browser.find_element_by_class_name("OutOfStockMsg")
 
         # Button isn't open restart the script
         print("Button isnt ready yet")
 
         # Refresh page after a delay
-        time.sleep(1)
+        time.sleep(5)
         browser.refresh()
 
     except:
 
-        addToCartBtn = addButton = browser.find_element_by_class_name("a-button-input")
+        addToCartBtn = addButton = browser.find_element_by_class_name("btn-primary")
 
         # Click the button and end the script
         print("Button was clicked")
